885a0a15c049b7ab61f70e9e7011d40821ae59c6
Timur Pliev <pliev@ursalab.ru>
2025-09-27 19:31:37 +0500

    lab1.1

--- src/labs/lab1/task1/Task1_3.java
+++ src/labs/lab1/task1/Task1_3.java
@@ -21,4 +21,23 @@ public class Task1_3 {
         System.out.println(charToNum(input));
     }
 
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    public double fraction (double x) {
+        return x % 1;
+    }
+    private double inputDouble () {
+        double input;
+        System.out.println("Введите дробное число: ");
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Это не дробное число!");
+            scanner.next();
+        }
+        input = scanner.nextDouble();
+        return input;
+    }
+    public void run() {
+        double input = inputDouble();
+        System.out.printf(Locale.US,"%.3f%n",fraction(input));
+    }
+
 }
