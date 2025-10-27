d492d8a875fe4a67d0b24ea8f83621b579e48de8
pliev-arthur <arthuprime@gmail.com>
2025-10-27 08:32:24 +0500

    lab4 task 1 and 2

--- src/ru/pliev/main/Main.java
+++ src/ru/pliev/main/Main.java
@@ -2,21 +2,19 @@ package ru.pliev.main;
 import ru.pliev.labs.lab1.*;
 import ru.pliev.labs.lab2.*;
 import ru.pliev.labs.lab3.*;
+import ru.pliev.labs.lab4.*;
+
 import static java.lang.Integer.parseInt;
 import static java.lang.Math.pow;
 
 public class Main {
     public static void main(String[] args) {
-//        if (args.length == 2) {
-//            double result = power(args[0], args[1]);
-//            System.out.println("Результат: " + result);
-//        }
-//        Lab3 lab3 =  new Lab3();
-//        lab3.run();
-//        Lab2 lab2 =  new Lab2();
-//        lab2.run();
-        Lab1 lab1 = new Lab1();
-        lab1.run();
+        if (args.length == 2) {
+            double result = power(args[0], args[1]);
+            System.out.println("Результат: " + result);
+        }
+        Lab4 lab4 =  new Lab4();
+        lab4.run();
     }
     public static double power(String xStr, String yStr) {
         int x = parseInt(xStr);
