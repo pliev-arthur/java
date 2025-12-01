215651bd05d193e06dea0c9984c38e74c26fdf4a
pliev-arthur <arthuprime@gmail.com>
2025-12-01 08:38:58 +0500

    lab6 1 2 3

--- src/ru/pliev/labs/lab6/task1_1/Invoke.java
+++ src/ru/pliev/labs/lab6/task1_1/Invoke.java
@@ -2,6 +2,34 @@ package ru.pliev.labs.lab6.task1_1;
 
 import java.lang.annotation.*;
 
+/**
+ * Аннотация для автоматического вызова методов через Reflection API.
+ * <p>
+ * Аннотация {@code @Invoke} предназначена для пометки методов, которые должны быть
+ * автоматически вызваны обработчиком {@link InvokeAnnotatedMethods} во время выполнения программы.
+ * </p>
+ *
+ * <p><b>Характеристики аннотации:</b></p>
+ * <ul>
+ *   <li>Цель применения: только МЕТОД</li>
+ *   <li>Доступна во время исполнения программы</li>
+ *   <li>Не имеет свойств</li>
+ * </ul>
+ *
+ * <p><b>Пример использования:</b></p>
+ * <pre>
+ * {@code
+ * public class ExampleClass {
+ *     @Invoke
+ *     public void annotatedMethod() {
+ *         System.out.println("Этот метод будет вызван автоматически");
+ *     }
+ * }
+ * }
+ * </pre>
+ *
+ * @see InvokeAnnotatedMethods
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Invoke {
